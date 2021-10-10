11696086d27fd8ab5396aeeee3128c0888351fb1
f-renout <frederic@renout.fr>
2021-10-10 20:36:40 +0200

    ajout chapitre 3

---
+++ chap3/2-javascript/boucles-for.js
@@ -0,0 +1,11 @@
+function bouclesFor() {
+    externe: for (var i = 0; i < 3; i++) {
+        console.log("i=" + i);
+        for (var j = 0; j < 3; j++) {
+            if (j === i) {
+                continue externe;
+            }
+            console.log("j=" + j);
+        }
+    }
+}
